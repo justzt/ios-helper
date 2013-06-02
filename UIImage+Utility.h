625c6eecf858b99a06b59cea81946dffb278c30e
justzt <justzt@gmail.com>
2013-06-02 08:18:20 +0800

    add UIImage+Utility.m

---
+++ UIImage+Utility.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Utility.h
+//  Pods
+//
+//  Created by justzt on 6/2/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Utility)
+- (UIImage*)imageRotatedByDegrees:(CGFloat)degrees;
+@end
